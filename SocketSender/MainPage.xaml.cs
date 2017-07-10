2f7c6dce9f1c8fb0462b8e96508705591c408294
Huy (Harold) Q. BUI <harold@enclave.vn>
2017-07-10 14:36:12 +0700

    init

---
+++ SocketSender/MainPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Networking;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SocketSender
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        StreamSocket socket;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            
+        }
+
+        private async Task SendSocketMessage()
+        {
+
+            object outValue;
+            DataWriter writer;
+            if (!CoreApplication.Properties.TryGetValue("clientDataWriter", out outValue))
+            {
+                writer = new DataWriter(socket.OutputStream);
+                CoreApplication.Properties.Add("clientDataWriter", writer);
+            }
+            else
+            {
+                writer = (DataWriter)outValue;
+            }
+            string stringToSend = "Team tao la Mateo";
+            writer.WriteUInt32(writer.MeasureString(stringToSend));
+            writer.WriteString(stringToSend);
+
+            // Write the locally buffered data to the network.
+            try
+            {
+                await writer.StoreAsync();
+            }
+            catch (Exception exception)
+            {
+                // If this is an unknown status it means that the error if fatal and retry will likely fail.
+            }
+        }
+
+        private async void Button_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            await SendSocketMessage();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            socket = new StreamSocket();
+            HostName hostName = new HostName("localhost");
+            socket.Control.KeepAlive = false;
+            await socket.ConnectAsync(hostName, "22112");
+        }
+    }
+}
